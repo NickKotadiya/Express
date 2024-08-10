3a112db154c52f7c73e267948c708703f854c0d7
NickKotadiya <nickkotadiya7751@email.com>
2024-08-10 18:40:28 +0530

    10-08-2024

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const server = express();   // create server
-// const data = require('./friend.json');
+const data = require('./friend.json');
 const fs = require('fs');
-const data = fs.readFileSync('./friend.json', 'utf8');
+const data = fs.readFileSync('./friend.json', 'utf-8');
 // console.log(data);
 
-// POST, GET PUT, PATCH, DELETE
-server.get('/', (req, res) => {
-    res.write('Welcome to Express');
-    res.end();
-})
+const myFun = (req, res, next) =>{
+    // console.log(req.query);
+    if(req.query.age >= 18){
+        console.log('Success');
+        next();
+    }else{
+        res.Json({message: "Sorry you are not allowed to visit this website ..."})
+    }
+}
 
+// server.use(myFun);  // application
+
+// POST, GET PUT, PATCH, DELETE
 // server.get('/', (req, res) => {
-//     res.write('Get Method - 1');
+//     res.write('Welcome to Express');
+//     res.end();
+// })
+
+// server.get('/login', myFun, (req, res) => {
+//     res.write({msg:'Welcome to Login Page'});
 //     res.end();
 // })
 
@@ -25,21 +37,18 @@ server.put('/', (req, res) => {
     res.json({msg:'Hello Put Method Called'});
 })
 
-server.patch('/', (req, res) => {
-    res.status(400);
-    res.json({msg:'Hello Patch Method Called'});
-})
+// server.patch('/', (req, res) => {
+//     res.status(400);
+//     res.json({msg:'Hello Patch Method Called'});
+// })
 
 server.get('/user', (req, res) => {
     res.json(JSON.parse(data));
 })
 
-server.get('/login', (req, res) => {
-    res.json({msg:'Welcome to Login Page'});
-    res.end();
-})
+
 
 
 server.listen(8000, () => {
-    console.log('Server Start at http://localhost:8000');
+    console.log('Server Start at http://localhost8000');
 });
