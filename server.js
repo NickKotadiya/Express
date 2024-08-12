d2847fbb681241817a8ff77282bbe940f2f353dc
NickKotadiya <nickkotadiya7751@email.com>
2024-08-12 14:53:48 +0530

    12-08-2024

--- server.js
+++ server.js
@@ -1,32 +1,76 @@
-const express = require("express");
-const app = express();
-const morgan = require("morgan");
-const users = require("./friend.json");
-console.log(users);
-app.use(morgan("dev"));
-app.use(express.json());
-app.use(express.urlencoded({extended:false }));
-app.get("/" , (req,res)=>{
-     res.send(" Welcome To Express Server");
-});
-// CRUD
-// Creat User
-     app.post("/users",(req,res)=>{
-     console.log(req.body);
-     users.push(req.body);
-     res.json({message: "User Added Success"});
-     });
-     // Read User  - Get All Users
-     app.get("/user",req,res=>{
-          res.json(users);
-     });
-     // Get Single User
-     app.get("/user/:id",(req,res)=>{
-          let id = +req.params.id;
-          let item = users.find((user)=>user.id === id)
-          res.json(item);
-     });
 
-     app.listen(2024,()=>{
-          console.log("Server Start at http://localhost:2024");
-     });
+// ******************* Lecture-5  ***********************
+const  express = require('express')
+const morgan = require('morgan')
+const app = express()
+const users = require('./friends.json')
+
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+
+app.get("/",(req,res)=>{
+  res.send("welcome to express server")
+})
+
+//CRUD
+//create 8user
+app.post("/users" ,(req,res)=>{
+  // console.log(req.body);
+  users.push(req.body)
+  res.json({message:"User added sucessfully......"})  
+})
+
+// READ -Get All users
+// -> first of all get run and than post method run to localhost/user => reply aaded msg after that open new tab in post man and get localhost/user => display aaed person and than single argemnet  localhost/user/11
+app.get("/users",(req,res) =>{
+  res.json(users)
+})
+
+//Get single User
+app.get("/users/:id" , (req,res) =>{
+  let id = +req.params.id;
+  let item = users.find((user)=> user.id === id)
+  res.json(item)
+})
+
+// Replace Data - PUT
+app.put("/users/:id",(req,res)=>{
+  let id = +req.params.id;
+  let userIndex = users.findIndex((item) => item.id === id)
+  users.splice(userIndex,1 , req.body)
+  res.json({message : "User Replaced Success"})
+})
+
+
+// Update Data - PATCH
+app.patch("/users/:id",(req,res)=>{
+  let id = +req.params.id;
+  let userIndex = users.findIndex((item) => item.id === id)
+  let user = users[userIndex]
+  users.splice(userIndex,1 , {...user ,...req.body})
+  res.json({message : "User update Success"})
+})
+
+
+//Delete Data - DELETE
+app.delete("/users/:id",(req,res)=>{
+  let id = +req.params.id;
+  let userIndex = users.findIndex((item) => item.id === id)
+  users.splice(userIndex,1)
+  res.json({message : "User delete Success"})
+})
+
+
+
+app.listen(8000 , () => {
+  console.log('Server start at http://localhost:8000');
+})
+
+
+
+//git checkout -b branch_name (create New branch)
+//git add .
+//git commit -m "your commit"
+//git push -u origin branch_name
