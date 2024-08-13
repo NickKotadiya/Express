f76023adc3b982b65257fda28c6f2608ca0a50f1
NickKotadiya <nickkotadiya7751@email.com>
2024-08-13 18:19:47 +0530

    13-08-2024

--- server.js
+++ server.js
@@ -1,55 +1,21 @@
-// ********************* Product Pectise ************************************
-const  express = require('express')
-const morgan = require('morgan')
-const app = express()
-const products = require("./product.json")
+// ---------------------------- CRUD OPERATION ON PRODUCT DUMMY DATA BY USING ROUTER ----------------------------
 
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+const express = require("express");
+const server = express();
+const morgan = require('morgan');
+const userRoutes = require("./routes/user.routes");
 
-app.get("/",(req,res)=>{
-  res.send("welcome to express product prectice task.....")
-})
+// Middleware
+server.use(morgan('short'));
+server.use(express.json());
+server.use(express.urlencoded({extended:false}));
 
-app.post('/products',(req,res)=>{
-  products.push(req.body)
-  res.json({message:"Product added succesfully......"})
-})
+server.get("/" , (req,res) => {
+    res.send("<h1>Welcome to server</h1>")
+} )
 
-app.get("/products",(req,res) =>{
-  res.json(products)
-})
+server.use("/product" , userRoutes);
 
-app.get("/products/:id" ,(req,res) => {
-  let id = +req.params.id
-  let item = products.find((product) => product.id === id)
-  res.json(item)
-})
-
-app.put("/products/:id" ,(req,res) => {
-  let id = +req.params.id
-  let productIndex = products.findIndex((item) => item.id === id)
-  products.splice(productIndex,1,req.body)
-  res.json({message : "Product Replaced Successfully....."})
-})
-
-app.patch("/products/:id" ,(req,res) => {
-  let id = +req.params.id
-  let productIndex = products.findIndex((item) => item.id === id)
-  let product = products[productIndex]
-  products.splice(productIndex,1, {...product , ...req.body})
-  res.json({message : "Product Updated Successfully....."})
-})
-
-app.delete("/products/:id" ,(req,res) => {
-  let id = +req.params.id
-  let productIndex = products.findIndex((item) => item.id === id)
-  products.splice(productIndex,1)
-  res.json({message : "Product Delete Successfully....."})
-})
-
-app.listen(4000,() => {
-  console.log(`server start at http://localhost:4000`);
-  
+server.listen(1122,() => {
+    console.log('Start at  http://localhost:2222 ');
 })
